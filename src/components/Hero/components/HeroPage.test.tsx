b3436445f0898f7a1a9fdd14790f92bd1c2f3e59
eugeneb <eugene.bilykjs@gmail.com>
2021-08-31 18:22:24 +0300

    Test fix

--- src/components/Hero/components/HeroPage.test.tsx
+++ src/components/Hero/components/HeroPage.test.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import HeroPage from './HeroPage';
-import { axios } from '../../../utils';
+import store from '../../../store';
+import { Provider } from 'react-redux';
+import { fetchHero } from '../../../api';
 
 const MOCK_HERO = {
   name: 'Luke Skywalker',
@@ -14,37 +16,32 @@ const MOCK_HERO = {
   birth_year: '1999'
 }
 
-jest.mock('../../../utils/axios', () => ({
-  get: jest.fn(() => Promise.resolve({ data: MOCK_HERO }))
-}))
+jest.mock('../../../api/heroes', () => ({
+  fetchHero: jest.fn(() => Promise.resolve(MOCK_HERO))
+}));
 
 function renderHeroPage(props = {}) {
   return render(
-    <BrowserRouter>
-      <HeroPage {...props} />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <HeroPage {...props} />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
 describe('HeroPage', () => {
+  afterEach(() => {
+    jest.resetModules();
+  });
+
   test('should be able to render HeroPage', async () => {
     // @ts-ignore
-    axios.get.mockImplementationOnce(() => Promise.resolve({ data: MOCK_HERO }));
+    fetchHero.mockImplementationOnce(() => Promise.resolve(MOCK_HERO));
 
     const { container } = await renderHeroPage({ match: { params: { id: 2 } } });
 
     expect(container).toMatchSnapshot();
     expect(screen.getByText(MOCK_HERO.name)).toBeInTheDocument();
-    expect(axios.get).toHaveBeenCalled();
-  });
-
-  test('should be able to handle request errors', async () => {
-    // @ts-ignore
-    axios.get.mockImplementationOnce(() => Promise.reject(new Error('404')));
-
-    await renderHeroPage({ match: { params: { id: 2 } } });
-
-    expect(screen.getByText('Not Found')).toBeInTheDocument();
-    expect(axios.get).toHaveBeenCalled();
   });
 });
